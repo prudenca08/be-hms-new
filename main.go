6d18a00714b13cf9952186d8cae4dc6b0291c0f5
prudenca08 <prudencaahdaffa08@gmail.com>
2022-01-10 22:34:23 +0700

    nyoba

---
+++ main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"log"
+
+	_routes "finalproject/routes"
+
+	_adminService "finalproject/features/admins/bussiness"
+	_adminRepo "finalproject/features/admins/data"
+	_adminController "finalproject/features/admins/presentation"
+
+	_doctorService "finalproject/features/doctor/bussiness"
+	_doctorRepo "finalproject/features/doctor/data"
+	_doctorController "finalproject/features/doctor/presentation"
+
+	_docsesService "finalproject/features/docses/bussiness"
+	_docsesRepo "finalproject/features/docses/data"
+	_docsesController "finalproject/features/docses/presentation"
+
+	_patientsesService "finalproject/features/patientses/bussiness"
+	_patientsesRepo "finalproject/features/patientses/data"
+	_patientsesController "finalproject/features/patientses/presentation"
+
+	_patscheService "finalproject/features/patsche/bussiness"
+	_patscheRepo "finalproject/features/patsche/data"
+	_patscheController "finalproject/features/patsche/presentation"
+
+	_patientService "finalproject/features/patient/bussiness"
+	_patientRepo "finalproject/features/patient/data"
+	_patientController "finalproject/features/patient/presentation"
+
+	_recipeService "finalproject/features/recipe/bussiness"
+	_recipeRepo "finalproject/features/recipe/data"
+	_recipeController "finalproject/features/recipe/presentation"
+
+	_dbDriver "finalproject/config"
+
+	_driverFactory "finalproject/drivers"
+
+	_middleware "finalproject/middleware"
+
+	"github.com/labstack/echo/v4"
+	"github.com/spf13/viper"
+	"gorm.io/gorm"
+)
+
+func init() {
+	viper.SetConfigFile(`config/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		log.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func dbMigrate(db *gorm.DB) {
+	db.AutoMigrate(
+		&_adminRepo.Admins{},
+		&_doctorRepo.Doctor{},
+		&_docsesRepo.Docses{},
+		&_patientRepo.Patient{},
+		&_patientsesRepo.Patientses{},
+		&_patscheRepo.Patsche{},
+		&_recipeRepo.Recipe{},
+	)
+}
+
+func main() {
+	configDB := _dbDriver.ConfigDB{
+		DB_Username: viper.GetString(`database.user`),
+		DB_Password: viper.GetString(`database.pass`),
+		DB_Host:     viper.GetString(`database.host`),
+		DB_Port:     viper.GetString(`database.port`),
+		DB_Database: viper.GetString(`database.name`),
+	}
+	db := configDB.InitDB()
+	dbMigrate(db)
+
+	configJWT := _middleware.ConfigJWT{
+		SecretJWT:       viper.GetString(`jwt.secret`),
+		ExpiresDuration: int64(viper.GetInt(`jwt.expired`)),
+	}
+
+	e := echo.New()
+
+	adminRepo := _driverFactory.NewAdminRepository(db)
+	adminService := _adminService.NewServiceAdmin(adminRepo, 10, &configJWT)
+	adminCtrl := _adminController.NewHandlerAdmin(adminService)
+
+	doctorRepo := _driverFactory.NewDoctorRepository(db)
+	doctorService := _doctorService.NewServiceDoctor(doctorRepo, 10, &configJWT)
+	doctorCtrl := _doctorController.NewHandlerDoctor(doctorService)
+
+	docsesRepo := _driverFactory.NewDocsesRepository(db)
+	docsesService := _docsesService.NewServiceDocses(docsesRepo)
+	docsesCtrl := _docsesController.NewHandlerDocses(docsesService)
+
+	patscheRepo := _driverFactory.NewPatscheRepository(db)
+	patscheService := _patscheService.NewServicePatsche(patscheRepo)
+	patscheCtrl := _patscheController.NewHandlerPatsche(patscheService)
+
+	patientRepo := _driverFactory.NewPatientRepository(db)
+	patientService := _patientService.NewServicePatient(patientRepo)
+	patientCtrl := _patientController.NewHandlerPatient(patientService)
+
+	patientsesRepo := _driverFactory.NewPatientsesRepository(db)
+	patientsesService := _patientsesService.NewServicePatientses(patientsesRepo)
+	patientsesCtrl := _patientsesController.NewHandlerPatientses(patientsesService)
+
+	recipeRepo := _driverFactory.NewRecipeRepository(db)
+	recipeService := _recipeService.NewServiceRecipe(recipeRepo)
+	recipeCtrl := _recipeController.NewHandlerRecipe(recipeService)
+
+	routesInit := _routes.RouteList{
+		JWTMiddleware: configJWT.Init(),
+		AdminRouter:   *adminCtrl,
+		DoctorRouter:  *doctorCtrl,
+		DocsesRouter:  *docsesCtrl,
+		PatientRouter: *patientCtrl,
+		PatientsesRouter: *patientsesCtrl,
+		PatscheRouter: *patscheCtrl,
+		RecipeRouter: *recipeCtrl,
+	}
+
+	routesInit.RouteRegister(e)
+
+	log.Fatal(e.Start(viper.GetString("server.address")))
+}
---
+++ routes/routes.go
@@ -0,0 +1,125 @@
+package routes
+
+import (
+	business "finalproject/features/admins/bussiness"
+	admins "finalproject/features/admins/presentation"
+	controller "finalproject/features/admins/presentation/response"
+
+	bussiness "finalproject/features/doctor/bussiness"
+	doctor "finalproject/features/doctor/presentation"
+	response "finalproject/features/doctor/presentation/response"
+
+	docses "finalproject/features/docses/presentation"
+
+	patsche "finalproject/features/patsche/presentation"
+
+	patient "finalproject/features/patient/presentation"
+
+	patientses "finalproject/features/patientses/presentation"
+
+	recipe "finalproject/features/recipe/presentation"
+
+	middlewareApp "finalproject/middleware"
+
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type RouteList struct {
+	JWTMiddleware middleware.JWTConfig
+	AdminRouter   admins.AdminHandler
+	DoctorRouter  doctor.DoctorHandler
+	DocsesRouter  docses.DocsesHandler
+	PatientRouter patient.PatientHandler
+	PatientsesRouter  patientses.PatientsesHandler
+	PatscheRouter patsche.PatscheHandler
+	RecipeRouter recipe.RecipeHandler
+}
+
+func (cl *RouteList) RouteRegister(e *echo.Echo) {
+	// Admins
+	admins := e.Group("admins")
+	admins.POST("/register", cl.AdminRouter.Register)
+	admins.POST("/login", cl.AdminRouter.Login)
+	admins.PUT("/update-doctor/:id", cl.DoctorRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.DELETE("/delete-doctor/:id", cl.DoctorRouter.Delete, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+
+	admins.POST("/create-patsche", cl.PatscheRouter.Create, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.PUT("/update-patsche/:id", cl.PatscheRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.DELETE("/delete-patsche/:id", cl.PatscheRouter.Delete, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+
+	admins.POST("/create-docses", cl.DocsesRouter.Create, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.PUT("/update-docses/:id", cl.DocsesRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.DELETE("/delete-docses/:id", cl.DocsesRouter.Delete, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+
+	admins.POST("/create-patientses", cl.PatientsesRouter.Create, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.PUT("/update-patientses/:id", cl.PatientsesRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.DELETE("/delete-patientses/:id", cl.PatientsesRouter.Delete, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	// doctor.PUT("/update-patientses/:id", cl.PatientsesRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationDoctor())
+
+	admins.POST("/create-patient", cl.PatientRouter.Create, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.PUT("/update-patient/:id", cl.PatientRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	admins.DELETE("/delete-patient/:id", cl.PatientRouter.Delete, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+
+	// Doctor
+	doctor := e.Group("doctor")
+	doctor.POST("/register", cl.DoctorRouter.Register, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationAdmin())
+	doctor.POST("/login", cl.DoctorRouter.Login)
+	doctor.PUT("/update-doctor/:id", cl.DoctorRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationDoctor())
+
+	doctor.POST("/create-recipe", cl.RecipeRouter.Create,  middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationDoctor())
+	doctor.PUT("/update-recipe" , cl.RecipeRouter.Update, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationDoctor())
+	doctor.DELETE("/delete-recipe/:id", cl.RecipeRouter.Delete, middleware.JWTWithConfig(cl.JWTMiddleware), RoleValidationDoctor())
+
+	//Docses
+	e.GET("/docses", cl.DocsesRouter.AllDocses)
+	e.GET("/docses/:id", cl.DocsesRouter.DocsesByID)
+
+	//Patients
+	e.GET("/patsche", cl.PatscheRouter.AllPatsche)
+	e.GET("/patsche/:id", cl.PatscheRouter.PatscheByID)
+
+	//Doctor
+	e.GET("/doctor", cl.DoctorRouter.AllDoctor)
+	e.GET("/doctor/:id", cl.DoctorRouter.DoctorByID)
+
+	//Docses
+	e.GET("/docses", cl.DocsesRouter.AllDocses)
+	e.GET("/docses/:id", cl.DocsesRouter.DocsesByID)
+
+	//Patientses
+	e.GET("/patientses",cl.PatientsesRouter.AllPatientses)
+
+	//Recipe
+	e.GET("/recipe", cl.RecipeRouter.AllRecipe)
+
+}
+
+func RoleValidationAdmin() echo.MiddlewareFunc {
+	return func(hf echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			claims := middlewareApp.GetUser(c)
+
+			if claims.Role == "admin" {
+				return hf(c)
+			} else {
+				return controller.NewErrorResponse(c, http.StatusForbidden, business.ErrUnathorized)
+			}
+		}
+	}
+}
+func RoleValidationDoctor() echo.MiddlewareFunc {
+	return func(hf echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			claims := middlewareApp.GetUser(c)
+
+			if claims.Role == "doctor" {
+				return hf(c)
+			} else {
+				return response.NewErrorResponse(c, http.StatusForbidden, bussiness.ErrUnathorized)
+			}
+		}
+	}
+}
